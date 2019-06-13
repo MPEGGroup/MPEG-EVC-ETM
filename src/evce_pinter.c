9722da40dd725ab547b2ade3a21d21e6d4ef486d
Roman Chernyak <chernyak.roman@huawei.com>
2019-06-13 16:11:33 +0300

    "test master_memset_fix"

--- src/evce_pinter.c
+++ src/evce_pinter.c
@@ -4718,7 +4718,7 @@ static double pinter_analyze_cu_baseline(EVCE_CTX *ctx, EVCE_CORE *core, int x, 
         s_rec[i] = (i == 0 ? cuw : cuw >> 1);
         evc_recon(pi->residue[i], pi->pred[best_idx][0][i], pi->nnz_best[best_idx][i], s_rec[i], (i == 0 ? cuh : cuh >> 1), s_rec[i], rec[i]);
         core->nnz[i] = pi->nnz_best[best_idx][i];
-        evc_mcpy(core->nnz_sub[i], pi->nnz_sub_best[best_idx][i], sizeof(int) * N_C * MAX_SUB_TB_NUM);
+        evc_mcpy(core->nnz_sub[i], pi->nnz_sub_best[best_idx][i], sizeof(int) * MAX_SUB_TB_NUM);
     }
 
     mi->pred_y_best = pi->pred[best_idx][0][0];
@@ -5732,7 +5732,7 @@ static double pinter_analyze_cu(EVCE_CTX *ctx, EVCE_CORE *core, int x, int y, in
         }
 #endif
         core->nnz[i] = pi->nnz_best[best_idx][i];
-        evc_mcpy(core->nnz_sub[i], pi->nnz_sub_best[best_idx][i], sizeof(int) * N_C * MAX_SUB_TB_NUM);
+        evc_mcpy(core->nnz_sub[i], pi->nnz_sub_best[best_idx][i], sizeof(int) * MAX_SUB_TB_NUM);
     }
 
     mi->pred_y_best = pi->pred[best_idx][0][0];
